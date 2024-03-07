4c2ac4ae34adbcda516b3e7322fc16f63d95adfb
guoerjun <guoerjun@sensetime.com>
2024-03-07 18:16:19 +0800

    fix

--- twtter.py
+++ twtter.py
@@ -1,16 +1,31 @@
 import tweepy
 
-from config import config
 
-# 获取用户名和密码
-# twtter_token = config["credentials"]['twtter_token']
-twtter_token = ''
+def connnect_to_twtter():
+    from config import config
 
-token = f"Bearer {twtter_token}"
-print("token:", token)
-client = tweepy.Client(bearer_token=twtter_token)
+    # 获取用户名和密码
+    # twtter_token = config["credentials"]['twtter_token']
+    twtter_token = ''
 
-user_info = client.get_users(usernames="SpaceX")
+    token = f"Bearer {twtter_token}"
+    print("token:", token)
+    client = tweepy.Client(bearer_token=twtter_token)
 
-for user in user_info.data:
-    print(user.username, user.profile_image_url)
+    user_info = client.get_users(usernames="SpaceX")
+
+    for user in user_info.data:
+        print(user.username, user.profile_image_url)
+
+def images_to_video(input_pattern,duration,fps=24,pix_fmt='yuv420p',s='1920×1080',c='libx264',f='mp4',b='4M'):
+    import ffmpeg
+    output_file = f"video_from_images.{f}"
+    (
+        ffmpeg.input(input_pattern, pattern_type='glob', framerate=fps)
+        .output(output_file, pix_fmt=pix_fmt,s=s,c=c,f=f,b=b,frames=duration*fps).run()
+    )
+
+    return output_file
+    
+
+
--- youtube.py
+++ youtube.py
@@ -1,9 +1,14 @@
 
 import subprocess
+import os
+
+target_dir = "./data"
 
 def dowload_webm(url):
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
     # 定义命令
-    command = f"yt-dlp --write-auto-subs --sub-lang zh-Hans,en -o '%(upload_date>%Y)s/%(title)s.%(ext)s' {url}"
+    command = f"yt-dlp --write-auto-subs --sub-lang zh-Hans,en -P '{target_dir}' -P 'temp:tmp' -P 'subtitle:subs' -o '%(uploader)s/%(title)s.%(ext)s' {url}"
     # 执行命令
     try:
         # 使用 subprocess.run() 方法执行命令
@@ -11,49 +16,108 @@ def dowload_webm(url):
         print("dowload success")
     except subprocess.CalledProcessError as e:
         print("download failed:", e)
+    
+    return 
 
-def pre_process_video(filepath):
+def dowload_video(url):
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+    from yt_dlp import YoutubeDL
+    ydl_opts = {
+        'outtmpl': f"{target_dir}/%(title)s.%(ext)s",
+        # 'writesubtitles': True,
+        'writeautomaticsub': True,
+        'subtitleslangs': ["zh-Hans","en"],
+        'postprocessors': [],
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+        info = ydl.extract_info(url, download=False)
+        video_file = ydl.prepare_filename(info)
+        subtitle_file_zh = video_file.rsplit('.', 1)[0] + '.zh-Hans.' + "vtt"
+        if not os.path.exists(subtitle_file_zh):
+            subtitle_file_zh = None
+        subtitle_file_en = video_file.rsplit('.', 1)[0] + '.en.' + "vtt"
+        if not os.path.exists(subtitle_file_en):
+            subtitle_file_en = None
+    print(video_file,subtitle_file_zh,subtitle_file_en)
+    return video_file,subtitle_file_zh,subtitle_file_en
+
+def pre_process_video(filepath,output_video=None,output_audio=None,output_subtitle=None,):
     import ffmpeg
-    output_audio = 'output_audio.mp3'
-    output_subtitle = 'output_subtitle.vtt'
-    output_video = 'output_video.webm'
+    output_audio = './data/output_audio.wav'
+    output_video = './data/output_video.webm'
 
     # 剥离音频
-    # ffmpeg.input(filepath).output(output_audio, acodec='mp3').run()
+    if output_audio is not None:
+        ffmpeg.input(filepath).output(output_audio, acodec='pcm_s16le').run()
 
     # 剥离字幕
-    ffmpeg.input(filepath).output(output_subtitle, f='webvtt').run()
+    if output_subtitle is not None:
+        ffmpeg.input(filepath).output(output_subtitle, f='webvtt').run()
+    # 裸视频流
+    if output_video is not None:
+        ffmpeg.input(filepath).output(output_video, vcodec='copy', an=None, sn=None).run()
 
-    ffmpeg.input(filepath).output(output_video, vcodec='copy', an=None, sn=None).run()
     return output_video,output_audio,output_subtitle
 
-def subtile_translate(filepath):
+def subtile_translate(src,dst):
     import webvtt
     from googletrans import Translator
     translator = Translator()
     
-    vtt = webvtt.read(filepath)
+    vtt = webvtt.read(src)
+    src_text = ""
     for caption in vtt:
-        print(caption.start)
-        print(caption.end)
-        print(caption.text)
-        translate_text = translator.translate(caption.text,src='en',dest='zh-cn') 
-        caption.text = translate_text.text
-        print(translate_text)
-    vtt.save("trans.vtt")
+        src_text += caption.text
 
-def merge_video_subtitle():
+    print(src_text)
+    translate_text = translator.translate(src_text,src='en',dest='zh-cn') 
+    dst_texts = translate_text.text.split("。")
+
+    del_idx = []
+    
+    for idx,caption in enumerate(vtt.captions):
+        if idx >= len(dst_texts) or dst_texts[idx] == "":
+            caption.text = ""
+            del_idx.append(idx)
+            continue
+        caption.text = dst_texts[idx]
+
+    print(len(vtt.captions),len(dst_texts),del_idx)
+    for i in reversed(del_idx):
+        del vtt.captions[i]
+
+    vtt.save(dst)
+    return dst
+
+def merge_video_subtitle(input_video,input_subtitle):
     import ffmpeg
-
-    input_video = 'output_audio.webm'
-    input_subtitle = 'trans.vtt'
     output_video = 'merge_video_subtitle.webm'
 
     ffmpeg.input(input_video).output(output_video, vf='subtitles=' + input_subtitle).run()
     return output_video
 
+def proccess_youtube_video(url):
+    video_path,subtitle_path_zh,subtitle_path_en = dowload_video(url)
+
+    if subtitle_path_zh is None and subtitle_path_en is not None:
+        subtitle_path_zh = video_path.rsplit('.', 1)[0] + '.zh-Hans.' + "vtt"
+        subtile_translate(subtitle_path_en,subtitle_path_zh)
+        merge_video_subtitle(video_path,subtitle_path_zh)
+
+    
+
+    
+
+
+
+
+
 if __name__ == "__main__":
-    dowload_webm("https://youtube.com/shorts/uvLsfzCEir8?feature=shared")
+    # proccess_youtube_video("https://youtu.be/MUqNwgPjJvQ?feature=shared")
+    pre_process_video("./data/Transformer models： Encoders.webm")
+    # dowload_webm("https://youtube.com/shorts/uvLsfzCEir8?feature=shared")
     # video,audio,subtile = pre_process_video("The Newest Computer Chips aren’t “Electronic” [iOXn4vqYOJA].webm")
     # subtile_translate("The Newest Computer Chips aren’t “Electronic” [iOXn4vqYOJA].en.vtt")
     # merge_video_subtitle()
